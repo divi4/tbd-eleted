3788e4a916c59b6a34c01bac0c34d9e902c029e0
Divi <david.martin1999@outlook.com>
2021-01-15 22:31:56 +1100

    Intial Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// var s = Snap("#svg");
+// var bigCircle = s.rect(150, 150, 100, 100);
+// bigCircle.attr({
+//     fill: "#bada55",
+//     stroke: "#000",
+//     strokeWidth: 5
+// });
+// bigCircle.transform("r15, 50, 50");
+//
+// // function rotate(){
+// //   ellipse.animate({ry:1}, 220, function(){
+// //     ellipse.animate({ry: 90}, 300);
+// //   });
+// };
